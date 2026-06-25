22b8158b936f217cf6030ebfa009b1555ddb67b7
Bonnibel <254572892+bbbonnibel@users.noreply.github.com>
2026-06-25 12:20:06 +0100

    build index.html off of readme.txt

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# carrion-userscripts
-Userscripts for carrion.chat.
+# Bonnibel's carrion userscripts
 
-[Read the doc site here.](https://bbbonnibel.github.io/carrion-userscripts/)
+These are small userscripts made for [carrion.chat](https://carrion.chat/) to improve quality of life.
+
+Each userscript (although there's just one right now) is small, so you can choose only the behaviour you want.
+
+[View source on GitHub](https://github.com/bbbonnibel/carrion-userscripts) (for nerds)
+
+##  Quality of life
+
+### [Dashboard Revised](https://bbbonnibel.github.io/carrion-userscripts/script/dashboard-revised/dashboard-revised.user.js)
+
+This script revises exclusively the dashboard page.
+
+- Adds the option of a compact view.
+- Notification center now has a Focus button, which filters the character grid down to only those characters with notifications.
--- build/docs.js
+++ build/docs.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs-extra");
+const showdown = require("showdown");
 const dir = require("./dir.js");
 const css = require("./lib/css.js");
 
@@ -9,13 +10,21 @@ async function build() {
   fs.ensureDirSync(dir.DIST);
   fs.ensureDirSync(path.join(dir.DIST, "styles"));
 
-  const static = [
-    "index.html",
-  ]
-  for (const filepath of static) {
-    fs.copyFileSync(path.join(dir.DOCS, filepath), path.join(dir.DIST, filepath));
+  // Build index.html
+  const README = fs.readFileSync(path.join(dir.CWD, "README.md"), { encoding: "utf-8" });
+  if (!README) {
+    throw Error("README is empty");
   }
+  const converter = new showdown.Converter();
+  converter.setFlavor("github");
+  let index = fs.readFileSync(path.join(dir.DOCS, "index.html"), { encoding: "utf-8" });
+  if (!index.includes("{{README}}")) {
+    throw Error("Index has no “{{README}}” marker");
+  }
+  index = index.replace("{{README}}", converter.makeHtml(README))
+  fs.writeFileSync(path.join(dir.DIST, "index.html"), index, { encoding: "utf-8" });
 
+  // Build styles
   const style = await css.compileSassFile(path.join(dir.DOCS, "styles/main.scss"), { allowNodeModules: true });
   fs.writeFileSync(path.join(dir.DIST, "styles/main.css"), style, { encoding: "utf-8" });
 
