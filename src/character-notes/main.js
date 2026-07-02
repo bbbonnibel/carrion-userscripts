0f3bd2827cb03377b1a9b49ff42a471d36b3d161
Bonnibel <254572892+bbbonnibel@users.noreply.github.com>
2026-07-02 21:09:52 +0100

    basic label getting/setting for characters

--- src/character-notes/main.js
+++ src/character-notes/main.js
@@ -26,6 +26,17 @@ function installStyle(css, origin, filename) {
 
 installStyle(mainCss, "character-notes", "main.css");
 
+//#region Utilities
+/**
+ * Filter an array down to only unique entries.
+ *
+ * @example array.filter(filterUnique);
+ */
+function filterUnique(value, index, array) {
+  return array.indexOf(value) === index;
+}
+//#endregion
+
 /**
  * @typedef {object} Character
  * @prop {string} id Character id
@@ -97,12 +108,143 @@ class CharacterBank {
     }
   }
 
-  /** Hard reset the character bank. Wipe storage, reset to zero. */
+  /**
+   * Re-save the character bank based on the current map.
+   */
+  save() {
+    this.list = [...this.map.values()];
+    this.cache = JSON.stringify(this.list);
+    GM_setValue(this.key, this.cache);
+  }
+
+  /**
+   * Hard reset the character bank. Wipe storage, reset to zero.
+   */
   hardReset() {
-    this.list = [];
     this.map = new Map();
-    this.cache = "[]";
-    GM_setValue(this.key, "[]");
+    this.save();
+  }
+
+  /**
+   * Delete a character from the bank.
+   *
+   * @param {string} id The character ID
+   */
+  deleteCharacter(id) {
+    this.map.delete(id);
+    this.save();
+  }
+
+  /**
+   * Does a character with this ID exist in our records?
+   *
+   * @param {string} id The character ID
+   * @returns Whether the character exists
+   */
+  exists(id) {
+    return this.map.has(id);
+  }
+
+  /**
+   * Validate a character record.
+   *
+   * This deletes the record if the character's name no longer matches, and does nothing otherwise.
+   * This exists to preserve carrion's privacy philosophy of name changes signalling a new unrelated character.
+   *
+   * @param {string} id The character's ID
+   * @param {string} name The character's current name.
+   */
+  validate(id, name) {
+    const character = this.getCharacter(id);
+    if (!character) {
+      return;
+    }
+    if (character.name === name) {
+      return;
+    }
+    this.deleteCharacter(id);
+  }
+
+  /**
+   * Create a character record.
+   * If a character already exists with this ID, this does nothing.
+   *
+   * @param {string} id The character ID.
+   * @param {string} name The character's current name.
+   */
+  createCharacter(id, name) {
+    if (this.map.has(id)) {
+      return;
+    }
+    this.map.set(id, { id, name, labels: [] });
+    this.save();
+  }
+
+  /**
+   * Ensure a character exists with this ID and name.
+   *
+   * This validates current records and creates the character if it doesn't exist.
+   *
+   * @param {string} id The character ID
+   * @param {string} name The character's name
+   */
+  ensureCharacter(id, name) {
+    this.validate(id, name);
+    if (!this.exists(id)) {
+      this.createCharacter(id, name);
+    }
+  }
+
+  /**
+   * Does this character have this label applied?
+   *
+   * @param {string} characterId The character ID
+   * @param {string} labelId The label ID
+   * @returns {boolean} True if the character has that label applied, false otherwise.
+   */
+  hasLabel(characterId, labelId) {
+    const character = this.getCharacter(characterId);
+    if (!character) {
+      return false;
+    }
+    return Boolean(character.labels.find((l) => l.id === labelId));
+  }
+
+  /**
+   * Add a label to a character's record.
+   * If the character doesn't exist, this does nothing.
+   * If the character already has this label, this does nothing.
+   *
+   * @param {string} characterId The character ID
+   * @param {string} labelId The label ID
+   */
+  addLabel(characterId, labelId) {
+    const character = this.getCharacter(characterId);
+    if (!character) {
+      return;
+    }
+    if (!this.hasLabel(characterId, labelId)) {
+      character.labels.push({ id: labelId });
+    }
+    this.save();
+  }
+
+  /**
+   * Remove a label from a character.
+   * If the character doesn't exist, this does nothing.
+   * If the character didn't have this label, this does nothing.
+   *
+   * @param {string} characterId The character ID
+   * @param {string} labelId The label ID
+   * @returns
+   */
+  removeLabel(characterId, labelId) {
+    const character = this.getCharacter(characterId);
+    if (!character) {
+      return;
+    }
+    character.labels = character.labels.filter((l) => l.id !== labelId);
+    this.save();
   }
 
   getCharacter(id) {
@@ -161,14 +303,12 @@ class LabelBank {
 
   /** Hard reset the label bank. Wipe storage, reset to zero. */
   hardReset() {
-    this.list = [];
     this.map = new Map();
-    this.cache = "[]";
-    GM_setValue(this.key, "[]");
+    this.save();
   }
 
   /**
-   * Re-save the character list based on the current map.
+   * Re-save the label list based on the current map.
    */
   save() {
     this.list = [...this.map.values()];
@@ -177,16 +317,38 @@ class LabelBank {
   }
 
   /**
+   * Delete a label from the bank.
    *
    * @param {string} id
    */
-  deleteCharacter(id) {
+  deleteLabel(id) {
     this.map.delete(id);
     this.save();
   }
 
   /**
+   * Create a new label.
+   *
+   * @param {{ icon: string, color: string, text: string }} config Config for the new label.
+   * @returns The created label, including its newly assigned ID.
+   */
+  createLabel(config) {
+    const id = crypto.randomUUID();
+    /** @type {Label} */
+    const label = {
+      id,
+      icon: config.icon,
+      color: config.color,
+      text: config.text,
+    };
+    this.map.set(id, label);
+    this.save();
+    return label;
+  }
+
+  /**
    * Get a label by ID.
+   *
    * @param {string} id
    */
   getLabel(id) {
@@ -194,7 +356,8 @@ class LabelBank {
   }
 
   /**
-   * Get a list of labels by ID>
+   * Get a list of labels by their IDs.
+   *
    * @param {string[]} ids
    */
   getLabels(ids) {
@@ -206,39 +369,37 @@ class NoteBank {
   // TODO
 }
 
-const characters = new CharacterBank();
-const labels = new LabelBank();
-const notes = new NoteBank();
+const characterBank = new CharacterBank();
+const labelBank = new LabelBank();
+const noteBank = new NoteBank();
 //#endregion
 
 //#region Core
 /**
  * Fetch the labels for a character.
- * @param {{ id: string, name: string }} lookup The character to look up.
+ * @param {{ id: string, name: string }} character The character to look up.
  * @returns {Label[]} The labels stored on the characters.
  */
 function fetchCharacterLabels(lookup) {
-  const { id, name } = lookup;
-  const character = characters.getCharacter(lookup.id);
-
-  // if (character.name !== lookup.name) {
-  //   characters.deleteCharacter(lookup.id);
-  //   return [];
-  // }
-
+  characterBank.validate(lookup.id, lookup.name);
+  const character = characterBank.getCharacter(lookup.id);
   if (!character) {
     return [];
   }
 
-  return labels.getLabels(character.labels.map((l) => l.id));
+  return labelBank.getLabels(character.labels.map((l) => l.id));
 }
 
 /**
  * Put a new label on a character.
- * @param {string} characterId
+ * @param {{ id: string, name: string }} character The character to put a label on. If they don't already exist, the name is used to create or update them.
  * @param {string} labelId
  */
-function putCharacterLabel(characterId, labelId) {}
+function putCharacterLabel(character, labelId) {
+  characterBank.ensureCharacter(character.id, character.name);
+
+  characterBank.addLabel(character.id, labelId);
+}
 //#endregion
 
 //#region Chat
@@ -266,19 +427,23 @@ function startCharacterPage() {
     });
   }
 
+  // debug setup
+  characterBank.hardReset();
+  labelBank.hardReset();
+  characterBank.ensureCharacter(characterId, characterName);
+  const label = labelBank.createLabel({ icon: "❤️", color: "red", text: "cool" });
+  putCharacterLabel({ id: characterId, name: characterName }, label.id);
+  // end debug setup
+
   const labels = fetchCharacterLabels({ id: characterId, name: characterName });
+  console.log(LOG_PREFIX, `Character ${characterId}/${characterName} has these labels:`, labels);
 }
 //#endregion
 
 function main() {
   console.log(LOG_PREFIX, "Starting");
-  characters.load();
-  labels.load();
-
-  console.debug(LOG_PREFIX, "Starting with these characters and labels.", {
-    characters: characters.list,
-    labels: labels.list,
-  });
+  characterBank.load();
+  labelBank.load();
 
   const pathname = window.location.pathname;
   if (pathname.startsWith("/character")) {
@@ -292,6 +457,11 @@ function main() {
   } else {
     console.warn(LOG_PREFIX, "Couldn't recognise which page to start.");
   }
+
+  console.debug(LOG_PREFIX, "Starting with these characters and labels.", {
+    characters: characterBank.list,
+    labels: labelBank.list,
+  });
 }
 
 main();
