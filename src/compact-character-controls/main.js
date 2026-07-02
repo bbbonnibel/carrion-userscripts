9030ee74d2eb7cbe5c5bcef4de9c851f492b8461
Bonnibel <254572892+bbbonnibel@users.noreply.github.com>
2026-07-02 23:15:23 +0100

    add compact character controls

--- src/compact-character-controls/main.js
+++ src/compact-character-controls/main.js
@@ -24,3 +24,110 @@ function installStyle(css, origin, filename) {
 }
 
 installStyle(mainCss, "compact-character-controls", "main.css");
+
+const menuBtn = template(`
+  <button type="button" class="btn btn-secondary bbb-compact-character-controls-btn">
+    <i class="fa-solid fa-ellipsis-vertical"></i>
+  </button>
+`);
+
+const contextMenu = template(`
+  <div class="bbb-compact-character-controls-menu"></div>
+`);
+
+const contextMenuHost = template(`
+  <div class="bbb-compact-character-controls-menu-host"></div>
+`); // boy these class names get long. good thing i only have to write them once!
+contextMenuHost.appendChild(contextMenu);
+
+function attachMenuEvents() {
+  const openClass = "open";
+
+  menuBtn.addEventListener("click", () => {
+    contextMenu.classList.toggle(openClass);
+  });
+
+  contextMenu.addEventListener("click", () => {
+    contextMenu.classList.remove(openClass);
+  });
+
+  document.body.addEventListener("click", (event) => {
+    const { target } = event;
+    if (!(target instanceof HTMLElement)) {
+      return;
+    }
+    if (target === menuBtn) {
+      // Clicking the menu button. Ignore.
+      return;
+    }
+    if (target === contextMenu || contextMenu.contains(target)) {
+      // Clicking the context menu or inside it. Ignore.
+      return;
+    }
+    contextMenu.classList.remove(openClass);
+  });
+}
+
+/**
+ * Install the menu controls in a container.
+ *
+ * @param {HTMLElement} container The element that will contain the menu controls.
+ */
+function installMenu(container) {}
+
+function startCharacterPage() {
+  const editBtn = document.querySelector("a.btn[href^='/edit']");
+  if (!editBtn) {
+    // Not our character.
+    return;
+  }
+
+  /** the container of the controls like the edit button. */
+  const characterControls = editBtn.parentElement;
+
+  /** the list of buttons to be moved. */
+  const importButtons = editBtn.parentElement.querySelectorAll(
+    ".btn[href^='/import']",
+  );
+
+  characterControls.appendChild(menuBtn);
+  menuBtn.insertAdjacentElement("afterend", contextMenuHost);
+  attachMenuEvents();
+  for (const button of importButtons) {
+    contextMenu.appendChild(button);
+    button.classList.remove("btn", "btn-secondary");
+  }
+}
+
+function startDashboardPage() {
+  const newCharacterBtn = document.querySelector("a.btn[href^='/create']");
+  if (!newCharacterBtn) {
+    // Mysteriously missing.
+    return;
+  }
+
+  /** the list of buttons to be moved. */
+  const importButtons = newCharacterBtn.parentElement.querySelectorAll(
+    ".btn[href^='/import']",
+  );
+
+  newCharacterBtn.insertAdjacentElement("afterend", menuBtn);
+  menuBtn.insertAdjacentElement("afterend", contextMenuHost);
+  attachMenuEvents();
+  for (const button of importButtons) {
+    contextMenu.appendChild(button);
+    button.classList.remove("btn", "btn-secondary");
+  }
+}
+
+function main() {
+  const pathname = window.location.pathname;
+  if (pathname.startsWith("/character")) {
+    startCharacterPage();
+  }
+  if (pathname.startsWith("/dashboard")) {
+    startDashboardPage();
+  }
+}
+
+main();
