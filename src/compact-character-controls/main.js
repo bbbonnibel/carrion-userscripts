d5ce52cc4f85dd49a1c3836aecef3879be4d7095
Bonnibel <254572892+bbbonnibel@users.noreply.github.com>
2026-07-02 22:23:16 +0100

    base

---
+++ src/compact-character-controls/main.js
@@ -0,0 +1,26 @@
+const mainCss = $import("./main.scss");
+
+/**
+ * @param {string} html The template element. Must be only one root element.
+ */
+function template(html) {
+  const t = document.createElement("div");
+  t.innerHTML = html;
+  return t.firstElementChild;
+}
+
+/**
+ * Install a style sheet into the document.
+ * @param {string} css The CSS content of this style element.
+ * @param {string} origin The origin of this style sheet. That's this script's name.
+ * @param {string} filename The name of this style sheet, e.g. "main.css"
+ */
+function installStyle(css, origin, filename) {
+  const e = document.createElement("style");
+  e.setAttribute("data-origin", origin);
+  e.setAttribute("data-filename", filename);
+  e.innerText = css;
+  document.head.appendChild(e);
+}
+
+installStyle(mainCss, "compact-character-controls", "main.css");
