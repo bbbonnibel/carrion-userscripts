2513f6dd6ec5c69d72c4f9f272f319639264c37c
Bonnibel <254572892+bbbonnibel@users.noreply.github.com>
2026-07-06 00:48:00 +0100

    add greasemonkey defs

---
+++ src/greasemonkey.js
@@ -0,0 +1,20 @@
+/** @typedef {null | undefined | object | string | number} GMPrimitive */
+
+/**
+ * Set the value of a key in the script's store.
+ *
+ * @global
+ * @param {string} key The name of the key whose value we're setting.
+ * @param {GMPrimitive} value The value to set to that key.
+ */
+function GM_setValue(key, value) {}
+
+/**
+ * Ge tthe value of a key from the script's store.
+ *
+ * @global
+ * @param {string} key The key whose value we're returning.
+ * @param {GMPrimitive} [defaultValue] A default value to be returned if the key doesn't exist in store.
+ * @returns {GMPrimitive | null} The value stored for this key, or else the default value.
+ */
+function GM_getValue(key, defaultValue) {}
