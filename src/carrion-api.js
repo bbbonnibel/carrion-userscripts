36d9e986eb829d53f803cce3d5dd702b8d8e6e08
Bonnibel <254572892+bbbonnibel@users.noreply.github.com>
2026-07-05 17:15:57 +0100

    update carrion api color definition

--- src/carrion-api.js
+++ src/carrion-api.js
@@ -1,6 +1,8 @@
+/** @typedef {("red"|"orange"|"yellow"|"green"|"blue"|"purple"|"pink"|"brown"|"teal"|"white"|"black"|"gray"|"dark_red"|"dark_orange"|"dark_yellow"|"dark_green"|"dark_blue"|"dark_purple"|"dark_pink"|"dark_brown"|"dark_teal")} Carrion.Api.V1.Color */
+
 /**
  * @typedef {object} Carrion.Api.V1.Character
  * @prop {number} id
  * @prop {string} name
- * @prop {Carrion.Color} name_color
+ * @prop {Carrion.Api.V1.Color} name_color
  */
