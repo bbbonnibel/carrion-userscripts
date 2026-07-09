56c9bccb52bd658becd73cb2585eff3d58dadbea
Bonnibel <254572892+bbbonnibel@users.noreply.github.com>
2026-07-09 13:19:38 +0100

    Character pin updates (#16)
    * make pin a button
    
    * tooltips
    
    * star full/empty on press, better focus behaviour
    
    * pin to end
    
    * stars have a drop shadow
    
    * fix svg rendering bug
    
    target the svg specifically to force it to redraw as purple
    
    * update readme
    
    * update version

--- src/character-pins/main.js
+++ src/character-pins/main.js
@@ -3,6 +3,15 @@ const starEmpty = $import("./star-empty.svg");
 const starFilled = $import("./star-filled.svg");
 const LOG_PREFIX = "[Character Pins]";
 
+/** This element is pinned. */
+const pinClass = "bbb-pinned";
+/** This element is pinned to the end. (It should also be marked pinned.) */
+const pinToEndClass = "bbb-pinned-to-end";
+/** Class for the pin button. */
+const pinBtnClass = "bbb-pin-character-button";
+/** The right mouse button is being pressed on this element. */
+const mbRightHeld = "bbb-mb-right-held";
+
 /**
  * @param {string} html The template element. Must be only one root element.
  */
@@ -30,6 +39,9 @@ installStyle(mainCss, "pinned-characters", "main.css");
 
 /** @typedef {HTMLDivElement} Card */
 
+/** @typedef {("start"|"end")} PinPosition */
+/** @typedef {Record<string, PinPosition>} PinRecord */
+
 /**
  * Convert a string list to a map.
  * @param {string[]} list A list of items, e.g. `["A", "B", "C"]`
@@ -44,16 +56,36 @@ function listToMap(list) {
 }
 
 /**
+ * Update the pin store to the current format.
+ */
+function updateStore() {
+  /** @type {string[] | PinRecord>} */
+  const pins = GM_getValue("pins", undefined);
+  if (Array.isArray(pins)) {
+    /** @type {PinRecord} */
+    const record = {};
+    for (const pin of pins) {
+      record[pin] = "start";
+    }
+    GM_setValue("pins", record);
+    console.debug(LOG_PREFIX, "Store updated.", {
+      old: pins,
+      new: record,
+    });
+  }
+}
+
+/**
  * Get pins from the store.
- * @returns {string[]} The list of pins
+ * @returns {PinRecord} The record of pins
  */
 function getPins() {
-  return GM_getValue("pins", []);
+  return GM_getValue("pins", {});
 }
 
 /**
  * Update the list of pins in the store.
- * @param {string[]} value The list of pins
+ * @param {PinRecord} value The record of pins
  */
 function setPins(value) {
   GM_setValue("pins", value);
@@ -79,7 +111,7 @@ function validatePins(cards) {
   const map = listToMap(names);
   const pins = getPins();
   let count = 0;
-  for (const pin of pins) {
+  for (const pin of Object.keys(pins)) {
     if (!map.has(pin)) {
       // This pin references a character who no longer exists.
       removePin(pin);
@@ -90,31 +122,81 @@ function validatePins(cards) {
 }
 
 /**
- * Add a character to the pin list.
+ * Remove a character from the pin list.
  * @param {string} name The character's name
  */
 function removePin(name) {
   console.debug(LOG_PREFIX, "Removing pin:", name);
   let pins = getPins();
-  pins = pins.filter((na) => na !== name);
+  delete pins[name];
   setPins(pins);
 }
 
 /**
- * Remove a character from the pin list.
+ * Add a character to the pin list.
  * @param {string} name The character's name
+ * @param {PinPosition} position The character's pin position
  */
-function addPin(name) {
-  console.debug(LOG_PREFIX, "Adding pin:", name);
+function savePin(name, position) {
+  console.debug(LOG_PREFIX, "Adding pin:", name, "to", position);
   let pins = getPins();
-  if (pins.includes(name)) {
-    return;
-  }
-  pins = [...pins, name];
+  pins[name] = position;
   setPins(pins);
+  console.debug(LOG_PREFIX, "Pins updated:", pins);
 }
 
-const pinClass = "bbb-pinned";
+/**
+ * Make a pin button.
+ * @returns {HTMLDivElement}
+ */
+function makePinBtn() {
+  const title_unpinned = "Click to pin to start, or right click to pin to end";
+  const title_pinned = "Click to unpin";
+  return template(`
+    <button
+      type="button"
+      class="${pinBtnClass}"
+      title="${title_unpinned}"
+      data-title-unpinned="${title_unpinned}";
+      data-title-pinned="${title_pinned}"
+    >
+      <div class="halo"></div>
+      <div class="star when-inactive">${starEmpty}</div>
+      <div class="star when-active">${starFilled}</div>
+    </button>
+  `);
+}
+
+/**
+ * Pin a card.
+ *
+ * This alters HTML only, not state. You also need to call {@link savePin} or {@link removePin}.
+ *
+ * @param {Card} card The character card
+ * @param {PinPosition | false} pin Which position to pin the card to, or `false` to unpin the card.
+ */
+function setCardPin(card, pin) {
+  const btn = card.querySelector(`.${pinBtnClass}`);
+  switch (pin) {
+    case "start":
+      card.classList.add(pinClass);
+      card.classList.remove(pinToEndClass);
+      btn.setAttribute("title", btn.getAttribute("data-title-pinned"));
+      break;
+    case "end":
+      card.classList.add(pinClass);
+      card.classList.add(pinToEndClass);
+      btn.setAttribute("title", btn.getAttribute("data-title-pinned"));
+      break;
+    case false:
+      card.classList.remove(pinClass);
+      card.classList.remove(pinToEndClass);
+      btn.setAttribute("title", btn.getAttribute("data-title-unpinned"));
+      break;
+    default:
+      throw Error(`Invalid pin value: "${pin}"`);
+  }
+}
 
 /**
  * Install a pin button in each characer card.
@@ -122,23 +204,42 @@ const pinClass = "bbb-pinned";
  */
 function installPinButtons(cards) {
   for (const card of cards) {
-    const pinBtn = template(`
-      <div class="bbb-pin-character-button">
-        <div class="halo"></div>
-        <div class="star hide-when-pinned">${starEmpty}</div>
-        <div class="star hide-unless-pinned">${starFilled}</div>
-      </div>
-    `);
+    const pinBtn = makePinBtn();
     card.insertBefore(pinBtn, card.firstChild);
     const name = getCharacterName(card);
 
-    pinBtn.addEventListener("click", () => {
+    pinBtn.addEventListener("mousedown", (event) => {
+      if (event.button === 2) {
+        pinBtn.classList.add(mbRightHeld);
+      }
+    });
+
+    pinBtn.addEventListener("mouseup", (event) => {
+      pinBtn.classList.remove(mbRightHeld);
+    });
+
+    pinBtn.addEventListener("mouseout", (event) => {
+      pinBtn.classList.remove(mbRightHeld);
+    });
+
+    pinBtn.addEventListener("click", (event) => {
       if (card.classList.contains(pinClass)) {
-        card.classList.remove(pinClass);
+        setCardPin(card, false);
         removePin(name);
       } else {
-        card.classList.add(pinClass);
-        addPin(name);
+        setCardPin(card, "start");
+        savePin(name, "start");
+      }
+    });
+
+    pinBtn.addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+      if (card.classList.contains(pinClass)) {
+        setCardPin(card, false);
+        removePin(name);
+      } else {
+        setCardPin(card, "end");
+        savePin(name, "end");
       }
     });
   }
@@ -146,18 +247,20 @@ function installPinButtons(cards) {
 
 function main() {
   console.debug(LOG_PREFIX, "Started.");
+  /** @type {Card[]} */
   const cards = [...document.querySelectorAll(".character-card")];
+  updateStore();
   validatePins(cards);
   installPinButtons(cards);
 
   // Init pins from store
   const pins = getPins();
   console.debug(LOG_PREFIX, "Initial pins:", pins);
-  const pinMap = listToMap(pins);
   for (const card of cards) {
     const name = getCharacterName(card);
-    if (pinMap.has(name)) {
-      card.classList.add(pinClass);
+    const pinPosition = pins[name];
+    if (pinPosition) {
+      setCardPin(card, pinPosition);
     }
   }
 }
