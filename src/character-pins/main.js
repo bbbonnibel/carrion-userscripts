f93e394ea290d548ab421a53f2a64cff0d2d688b
Bonnibel <254572892+bbbonnibel@users.noreply.github.com>
2026-07-09 01:57:08 +0100

    Character pins (#14)
    * add pin character behaviour
    
    * persist pin data
    
    * readme

---
+++ src/character-pins/main.js
@@ -0,0 +1,165 @@
+const mainCss = $import("./main.scss");
+const starEmpty = $import("./star-empty.svg");
+const starFilled = $import("./star-filled.svg");
+const LOG_PREFIX = "[Character Pins]";
+
+/**
+ * @param {string} html The template element. Must be only one root element.
+ */
+function template(html) {
+  const t = document.createElement("div");
+  t.innerHTML = html;
+  return t.firstElementChild;
+}
+
+/**
+ * Install a style sheet into the document.
+ * @param {string} css The CSS content of this style element.
+ * @param {string} origin The origin of this style sheet. That's this script's name.
+ * @param {string} filename The name of this style sheet, e.g. "main.css"
+ */
+function installStyle(css, origin, filename) {
+  const e = document.createElement("style");
+  e.setAttribute("data-origin", origin);
+  e.setAttribute("data-filename", filename);
+  e.innerText = css;
+  document.head.appendChild(e);
+}
+
+installStyle(mainCss, "pinned-characters", "main.css");
+
+/** @typedef {HTMLDivElement} Card */
+
+/**
+ * Convert a string list to a map.
+ * @param {string[]} list A list of items, e.g. `["A", "B", "C"]`
+ * @returns {Map<string, true>} A map of those items, true for each one in the list: `Map[{ A: true, B: true, C: true }]`
+ */
+function listToMap(list) {
+  const map = new Map();
+  for (const item of list) {
+    map.set(item, true);
+  }
+  return map;
+}
+
+/**
+ * Get pins from the store.
+ * @returns {string[]} The list of pins
+ */
+function getPins() {
+  return GM_getValue("pins", []);
+}
+
+/**
+ * Update the list of pins in the store.
+ * @param {string[]} value The list of pins
+ */
+function setPins(value) {
+  GM_setValue("pins", value);
+}
+
+/**
+ * Get the character name on this card.
+ * @param {Card} card The character's card
+ * @returns {string} The character's name
+ */
+function getCharacterName(card) {
+  return card.querySelector(".character-name").textContent.trim();
+}
+
+/**
+ * Validate the pins so that only existing characters stay pinned.
+ * If a character is pinned but no longer exists, their pin gets removed.
+ * @param {Card[]} cards The current set of character cards
+ */
+function validatePins(cards) {
+  console.debug(LOG_PREFIX, "Validating pins");
+  const names = cards.map((card) => getCharacterName(card));
+  const map = listToMap(names);
+  const pins = getPins();
+  let count = 0;
+  for (const pin of pins) {
+    if (!map.has(pin)) {
+      // This pin references a character who no longer exists.
+      removePin(pin);
+      count++;
+    }
+  }
+  console.debug(LOG_PREFIX, "Validation has removed", count, "pins");
+}
+
+/**
+ * Add a character to the pin list.
+ * @param {string} name The character's name
+ */
+function removePin(name) {
+  console.debug(LOG_PREFIX, "Removing pin:", name);
+  let pins = getPins();
+  pins = pins.filter((na) => na !== name);
+  setPins(pins);
+}
+
+/**
+ * Remove a character from the pin list.
+ * @param {string} name The character's name
+ */
+function addPin(card) {
+  console.debug(LOG_PREFIX, "Adding pin:", name);
+  let pins = getPins();
+  if (pins.includes(name)) {
+    return;
+  }
+  pins = [...pins, name];
+  setPins(pins);
+}
+
+const pinClass = "bbb-pinned";
+
+/**
+ * Install a pin button in each characer card.
+ * @param {Card[]} cards The dashboard's character cards
+ */
+function installPinButtons(cards) {
+  for (const card of cards) {
+    const pinBtn = template(`
+      <div class="bbb-pin-character-button">
+        <div class="halo"></div>
+        <div class="star hide-when-pinned">${starEmpty}</div>
+        <div class="star hide-unless-pinned">${starFilled}</div>
+      </div>
+    `);
+    card.insertBefore(pinBtn, card.firstChild);
+    const name = getCharacterName(card);
+
+    pinBtn.addEventListener("click", () => {
+      if (card.classList.contains(pinClass)) {
+        card.classList.remove(pinClass);
+        removePin(name);
+      } else {
+        card.classList.add(pinClass);
+        addPin(name);
+      }
+    });
+  }
+}
+
+function main() {
+  console.debug(LOG_PREFIX, "Started.");
+  const cards = [...document.querySelectorAll(".character-card")];
+  validatePins(cards);
+  installPinButtons(cards);
+
+  // Init pins from store
+  const pins = getPins();
+  console.debug(LOG_PREFIX, "Initial pins:", pins);
+  const pinMap = listToMap(pins);
+  for (const card of cards) {
+    const name = getCharacterName(card);
+    if (pinMap.has(name)) {
+      card.classList.add(pinClass);
+    }
+  }
+}
+
+main();
