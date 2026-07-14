f348ce0240bc61c1ae69a4d59d6a8086e6e375b7
Bonnibel <254572892+bbbonnibel@users.noreply.github.com>
2026-07-14 21:15:27 +0100

    add hashlinks to docs

--- build/docs.js
+++ build/docs.js
@@ -3,16 +3,12 @@ const fs = require("fs-extra");
 const showdown = require("showdown");
 const dir = require("./dir.js");
 const css = require("./lib/css.js");
+const { JSDOM } = require("jsdom");
 
-async function build() {
-  process.stdout.write(`Building docs...\n`);
-
-  fs.ensureDirSync(dir.DIST);
-  fs.copySync(path.join(dir.CWD, "docs"), dir.DIST);
-  fs.ensureDirSync(path.join(dir.DIST, "styles"));
-  fs.emptyDirSync(path.join(dir.DIST, "styles"));
-
-  // Build index.html
+/**
+ * Load README.md parsed as a HTML document.
+ */
+function loadReadme() {
   const README = fs.readFileSync(path.join(dir.CWD, "README.md"), {
     encoding: "utf-8",
   });
@@ -22,18 +18,46 @@ async function build() {
   const converter = new showdown.Converter();
   converter.setOption("headerIds", false);
   converter.setFlavor("github");
-  let index = fs.readFileSync(path.join(dir.DOCS, "index.html"), {
+  const readme = converter.makeHtml(README);
+  return readme;
+}
+
+/**
+ * Build the docs HTML file.
+ */
+function buildHtml() {
+  const readme = loadReadme();
+
+  const indexRaw = fs.readFileSync(path.join(dir.DOCS, "index.html"), {
     encoding: "utf-8",
   });
-  if (!index.includes("{{README}}")) {
-    throw Error("Index has no “{{README}}” marker");
-  }
-  index = index.replace("{{README}}", converter.makeHtml(README));
-  fs.writeFileSync(path.join(dir.DIST, "index.html"), index, {
-    encoding: "utf-8",
+  const dom = new JSDOM(indexRaw);
+  const body = dom.window.document.body;
+  const main = body.querySelector("main");
+  main.innerHTML = readme;
+  body.querySelectorAll("h1, h2, h3").forEach((heading) => {
+    const text =
+      heading.textContent?.trim().toLowerCase() ??
+      // heading.querySelector("a")?.innerText?.trim().toLowerCase() ??
+      "";
+    const id = text
+      .replaceAll("'", "")
+      .replaceAll(/[^a-z]/g, "-")
+      .replaceAll(/-{2,}/g, "-");
+    heading.setAttribute("id", id);
+    const hashlink = dom.window.document.createElement("a");
+    hashlink.classList.add("hashlink");
+    hashlink.href = `#${id}`;
+    hashlink.textContent = "＃";
+    heading.prepend(hashlink);
   });
 
-  // Build styles
+  fs.writeFileSync(path.join(dir.DIST, "index.html"), dom.serialize(), {
+    encoding: "utf-8",
+  });
+}
+
+async function buildStyles() {
   const style = await css.compileSassFile(
     path.join(dir.DOCS, "styles/main.scss"),
     { allowNodeModules: true },
@@ -42,6 +66,18 @@ async function build() {
     encoding: "utf-8",
   });
   fs.removeSync(path.join(dir.DIST, "styles/main.scss"));
+}
+
+async function build() {
+  process.stdout.write(`Building docs...\n`);
+
+  fs.ensureDirSync(dir.DIST);
+  fs.copySync(path.join(dir.CWD, "docs"), dir.DIST);
+  fs.ensureDirSync(path.join(dir.DIST, "styles"));
+  fs.emptyDirSync(path.join(dir.DIST, "styles"));
+
+  buildHtml();
+  await buildStyles();
 
   process.stdout.write(` ✓ Built docs\n`);
 }
