dddbf092ecdc36e91fb88227fdd1ef3d2a6f405d
Bonnibel <254572892+bbbonnibel@users.noreply.github.com>
2026-08-28 13:01:19 +0100

    levenshtein distance sort near matches in user autocomplete

--- src/autocomplete/main.js
+++ src/autocomplete/main.js
@@ -29,6 +29,115 @@ function installStyle(css, origin, filename) {
 installStyle(mainCss, "autocomplete", "main.css");
 //#endregion
 
+//#region Levenshtein distance
+/**
+ * Calculate the levenshtein distance between two words.
+ * @param {string} a
+ * @param {string} b
+ * @returns {number} The levenshtein distance between `a` and `b`.
+ */
+const levenshtein = (function () {
+  function _min(d0, d1, d2, bx, ay) {
+    return d0 < d1 || d2 < d1
+      ? d0 > d2
+        ? d2 + 1
+        : d0 + 1
+      : bx === ay
+        ? d1
+        : d1 + 1;
+  }
+
+  return function (a, b) {
+    if (a === b) {
+      return 0;
+    }
+
+    if (a.length > b.length) {
+      var tmp = a;
+      a = b;
+      b = tmp;
+    }
+
+    var la = a.length;
+    var lb = b.length;
+
+    while (la > 0 && a.charCodeAt(la - 1) === b.charCodeAt(lb - 1)) {
+      la--;
+      lb--;
+    }
+
+    var offset = 0;
+
+    while (offset < la && a.charCodeAt(offset) === b.charCodeAt(offset)) {
+      offset++;
+    }
+
+    la -= offset;
+    lb -= offset;
+
+    if (la === 0 || lb < 3) {
+      return lb;
+    }
+
+    var x = 0;
+    var y;
+    var d0;
+    var d1;
+    var d2;
+    var d3;
+    var dd;
+    var dy;
+    var ay;
+    var bx0;
+    var bx1;
+    var bx2;
+    var bx3;
+
+    var vector = [];
+
+    for (y = 0; y < la; y++) {
+      vector.push(y + 1);
+      vector.push(a.charCodeAt(offset + y));
+    }
+
+    var len = vector.length - 1;
+
+    for (; x < lb - 3; ) {
+      bx0 = b.charCodeAt(offset + (d0 = x));
+      bx1 = b.charCodeAt(offset + (d1 = x + 1));
+      bx2 = b.charCodeAt(offset + (d2 = x + 2));
+      bx3 = b.charCodeAt(offset + (d3 = x + 3));
+      dd = x += 4;
+      for (y = 0; y < len; y += 2) {
+        dy = vector[y];
+        ay = vector[y + 1];
+        d0 = _min(dy, d0, d1, bx0, ay);
+        d1 = _min(d0, d1, d2, bx1, ay);
+        d2 = _min(d1, d2, d3, bx2, ay);
+        dd = _min(d2, d3, dd, bx3, ay);
+        vector[y] = dd;
+        d3 = d2;
+        d2 = d1;
+        d1 = d0;
+        d0 = dy;
+      }
+    }
+
+    for (; x < lb; ) {
+      bx0 = b.charCodeAt(offset + (d0 = x));
+      dd = ++x;
+      for (y = 0; y < len; y += 2) {
+        dy = vector[y];
+        vector[y] = dd = _min(dy, d0, dd, bx0, vector[y + 1]);
+        d0 = dy;
+      }
+    }
+
+    return dd;
+  };
+})();
+//#endregion
+
 //#region Utilities
 /**
  * Convert a class `string` to a class selector `.string`
@@ -816,24 +925,60 @@ function parseMention() {
     return;
   }
 
-  /** @type {string[]} */
-  const bookmarks = unsafeWindow.socialManager
-    .getBookmarks()
-    .map((b) => b.name);
-  /** @type {string[]} */
-  const onlineUsers = unsafeWindow.drakensberg.getOnlineUsers();
-
-  /** The set of users available for autocomplete. */
-  const users = [...bookmarks, ...onlineUsers].filter(filterUnique);
-
+  /** The normalized mention being entered. */
   let mention = word.segment.slice(1).toLowerCase();
   if (mention.startsWith("[")) {
     mention = mention.slice(1);
   }
 
-  const options = users
-    .filter((u) => u.toLowerCase().replaceAll(" ", "").includes(mention))
-    .toSorted(sortAlphabetic);
+  /** @type {string[]} The list of names the user has bookmarked. */
+  const bookmarks = unsafeWindow.socialManager
+    .getBookmarks()
+    .map((b) => b.name);
+  /** @type {string[]} The list of names of people online. */
+  const onlineUsers = unsafeWindow.drakensberg.getOnlineUsers();
+
+  /** The set of users available for autocomplete. */
+  const users = [...bookmarks, ...onlineUsers]
+    .filter(filterUnique)
+    .map((name) => {
+      return {
+        name,
+        normalized: name.toLowerCase().replaceAll(" ", ""),
+      };
+    });
+
+  const near = [];
+  const far = [];
+  for (const user of users) {
+    if (!user.normalized.includes(mention)) {
+      continue;
+    }
+    if (user.normalized.length - mention.length <= 8) {
+      near.push(user.name);
+    } else {
+      far.push(user.name);
+    }
+  }
+
+  const options = [
+    ...near
+      .map((name) => {
+        return {
+          name,
+          distance: levenshtein(mention, name.toLowerCase()),
+        };
+      })
+      .toSorted((a, b) => {
+        const difference = a.distance - b.distance;
+        if (difference !== 0) {
+          return difference;
+        }
+        return sortAlphabetic(a.name.toLowerCase(), b.name.toLowerCase());
+      })
+      .map((record) => record.name),
+    ...far.toSorted(sortAlphabetic),
+  ];
 
   const elements = options.map((option) => {
     const li = makeUsernameAutocompleteOption(option);
@@ -909,7 +1054,6 @@ function bindKeyboardManagementEvents() {
           autocomplete.keyboardFocusUp();
           break;
         case "Tab":
-        case "Enter":
           event.preventDefault();
           autocomplete.pickFocusedOption();
           break;
